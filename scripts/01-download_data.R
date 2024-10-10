69049c13a4f94f938a24fb96eb240f325ed4b78b
Yanzun Jiang <stary54264@gmail.com>
2024-10-10 16:05:04 -0400

    Update

--- scripts/01-download_data.R
+++ scripts/01-download_data.R
@@ -1,7 +1,7 @@
 #### Preamble ####
-# Purpose: Downloads and saves the data from Open Data Toronto
+# Purpose: Downloads and saves the data from Spotify
 # Author: Yanzun Jiang
-# Date: 24 September 2024
+# Date: 10 October 2024
 # Contact: yanzun.jiang@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: None
@@ -14,18 +14,8 @@ library(tidyverse)
 
 
 #### Download data ####
-# Get package
-package <- show_package("toronto-beaches-observations")
-
-# Get all resources for this package
-resources <- list_package_resources("toronto-beaches-observations")
-
-# Identify datastore resources; by default, Toronto Open Data sets datastore resource format to CSV for non-geospatial and GeoJSON for geospatial resources
-datastore_resources <- filter(resources, tolower(format) %in% c('csv', 'geojson'))
-
-# Load the first datastore resource as a sample
-data <- filter(datastore_resources, row_number()==1) %>% get_resource()
+david_tao <- get_artist_audio_features("David Tao")
 
 
 #### Save data ####
-write_csv(data, "data/raw_data/raw_data.csv")
+saveRDS(david_tao, "data/raw_data/david_tao.rds")
